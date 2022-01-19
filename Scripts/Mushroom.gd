2c6af6820ddb318bbc1bbbad6bf581dd40afaeb3
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2022-01-19 20:45:18 -0300

    Adicionando hitboxes ao inimigo e hurtboxes ao jogador

--- Scripts/Mushroom.gd
+++ Scripts/Mushroom.gd
@@ -2,28 +2,52 @@ extends KinematicBody2D
 
 export var speed = 64
 export var health = 1
-var velocity = Vector2.ZERO
+var motion = Vector2.ZERO
 var gravity = 1200
 var move_direction = -1
+var hitted = false
 
 func _physics_process(delta: float) -> void:
-	velocity.x = speed * move_direction
+	motion.x = speed * move_direction
+	motion.y += gravity * delta
 	
 	if move_direction == 1:
 		$texture.flip_h = true
 	else:
 		$texture.flip_h = false
 	
-	if $ray_wall.is_colliding():
-		$anim.play("idle")
+	_set_animation()
 	
-	velocity = move_and_slide(velocity)
+	motion = move_and_slide(motion)
 
 func _on_anim_animation_finished(anim_name: String) -> void:
 	if anim_name == "idle":
 		$texture.flip_h != $texture.flip_h
-		
 		$ray_wall.scale.x *= -1
 		move_direction *= -1
-		
 		$anim.play("run") 
+
+
+func _set_animation():
+	var anim = "run"
+	
+	if $ray_wall.is_colliding():
+		anim = "idle"
+	elif motion.x != 0:
+		anim = "run"
+	
+	if hitted:
+		anim = "hit"
+	
+	if $anim.assigned_animation != anim:
+		$anim.play(anim)
+
+func _on_hitbox_body_entered(body: Node) -> void:
+	hitted = true
+	health -= 1
+	body.velocity.y -= 300
+	yield(get_tree().create_timer(0.2), "timeout")
+	hitted = false
+	if health < 1:
+		queue_free()
+		get_node("hitbox/collision").set_deferred("disabled", true)
