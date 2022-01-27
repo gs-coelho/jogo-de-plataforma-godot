c3df77fcc47a9b14f28b5573f462d681f4117344
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2022-01-27 16:01:18 -0300

    Criando um HUD que contabiliza os pontos das frutas

--- Scripts/Items.gd
+++ Scripts/Items.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+export var fruits = 1
 
 func _on_items_body_entered(body: Node) -> void:
 	$anim.play("collected")
+	Global.fruits += fruits
 
 
 func _on_anim_animation_finished(anim_name: String) -> void:
